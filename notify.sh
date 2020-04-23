a7c1627b425584305a3f75f7f6e6340369a7896c
Mads Boddum <danish_cyborg@hotmail.com>
2020-04-23 23:43:06 +0200

    Movie title is now displayed before event type

--- notify.sh
+++ notify.sh
@@ -7,12 +7,12 @@ curl -X POST $1 \
 			"color": 16761392,
 			"fields": [
 				{
-					"name": "Event",
-					"value": "${radarr_eventtype}"
-				},
-				{
 					"name": "Movie",
 					"value": "${radarr_movie_title}"
+				},
+				{
+					"name": "Event",
+					"value": "${radarr_eventtype}"
 				},
 				{
 					"name": "Quality",
